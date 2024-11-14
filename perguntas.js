838fafa198fcfa252aedf5c7911b82d53374a041
esheleybelmiro <belmiroesheley@gmail.com>
2024-11-14 16:55:03 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,21 +1,19 @@
 criaCartao(
-  'Programação',
-  'O que é Python?',
-  'O Python é uma linguagem de programação'
-
-  )
-criaCartao(
-  'Geografia',
-  'Qual a capital da França?'
-  'A capital da França é Paris'
+'Programação',
+'O que é Python?',
+'O PythoncriaCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris'
+) é uma linguagem de programação'
 )
 criaCartao(
-  'Programação',
-  O que é uma função?',
-  'uma função é um bloco de código que executa alguma tarefa'
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
 )
 criaCartao(
-  'Lingua inglesa',
-  'Como se diz oi em Inglês?,
-  'Oi em Inglês é HI (RAI)'
+'Lingua inglesa',
+'Como se diz oi em Inglês?',
+'Oi em ingles é HI (RAI)'
 )
